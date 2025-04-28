369d86e93f4d5e27f9ea5b3da4beb062bccddcb0
Jose David <asefadoj@gmail.com>
2025-04-28 00:30:38 -0300

    first commit

---
+++ tp4-ej1.py
@@ -0,0 +1,22 @@
+"""
+Ejercicio Nro 1
+Escribir un programa que almacene diferentes lenguajes
+de programación (Python, Java, JavaScript, Haskell, 
+Kotlin, 
+C++, Ruby, Lua, PHP, 
+Swift, SQL) en una lista 
+y muestre por pantalla el siguiente mensaje: 
+"Yo estudio, {nombre_del_lenguaje}" para todos estos.
+"""
+
+lista = ["Python", "Java", "JavaScript", "Haskell",
+         "C++", "Ruby", "Lua", "PHP"]
+"""
+for i in lista:
+    #Aqui se muestra el elemento directamente
+    print(f"Yo estudio {i}") 
+"""
+for i in range(len(lista)):
+    #Aqui se muestra el elemento a traves del indice
+    print(f"Yo estudio {lista[i]}")
+
