0910f052626064f67ec5e89145ba66120ca00049
Jose David <asefadoj@gmail.com>
2025-05-01 09:24:46 -0300

    Ejercicio 4

---
+++ tp4-ej3.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio Nro 3
+Escribir un programa que almacene en una lista 
+los números del 1 al 10 y los muestre 
+por pantalla en orden inverso.
+"""
+lista = []
+for i in range(1, 10+1):
+    lista.append(i)
+
+for i in range(10,0,-1):
+    print(i)
+
+    
+
