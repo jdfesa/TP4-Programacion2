bde27c0df5064fdca238330add0e07d3a361aab4
Jose David <asefadoj@gmail.com>
2025-05-02 17:43:30 -0300

    Agregando comentarios ejercicio1

--- tp4-ej1.py
+++ tp4-ej1.py
@@ -11,11 +11,14 @@ y muestre por pantalla el siguiente mensaje:
 
 lista = ["Python", "Java", "JavaScript", "Haskell",
          "C++", "Ruby", "Lua", "PHP"]
+
 """
 for i in lista:
     #Aqui se muestra el elemento directamente
     print(f"Yo estudio {i}") 
+    # print(f"{lista[i]}") # NO hacer esto cuando uso in
 """
+
 for i in range(len(lista)):
     #Aqui se muestra el elemento a traves del indice
     print(f"Yo estudio {lista[i]}")
